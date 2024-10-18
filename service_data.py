41bdf04dbee075f304586f537c5ec48f5445f0cc
Király Zoltán <kiralyzoltan@DE-H3P3353.duna.local>
2024-10-19 00:26:14 +0200

    Frissített Dockerfile és Python kód

--- service_data.py
+++ service_data.py
@@ -27,4 +27,4 @@ def get_servicenow_data():
         return jsonify({"error": "Failed to retrieve assignment groups", "details": response.text}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  
+    app.run(debug=True, host='0.0.0.0', port=5010)  
